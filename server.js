323c3be1a977f29ab0b2467594261be0975b6705
chaitanya <yourchaitu@gmail.com>
2016-03-01 17:25:49 +0530

    View change

--- server.js
+++ server.js
@@ -7,56 +7,56 @@ const server = new Hapi.Server();
 
 
 server.connection({ 
-    host: '0.0.0.0', 
-    port: 8000 
+ host: '0.0.0.0', 
+ port: 8000 
 });
 
 const io = require('socket.io')(server.listener);
 
 server.register(require('inert'), (err) => {
 
-    if (err) {
-        throw err;
-    }
+ if (err) {
+  throw err;
+ }
 
 
 
 
 // Add the route
 server.route({
-        method: 'GET',
-        path: '/',
-        handler: function (request, reply) {
-            reply.file('index.html');
-        }
-    });
+ method: 'GET',
+ path: '/',
+ handler: function (request, reply) {
+  reply.file('index.html');
+ }
+});
 var connectCounter=0;
 var usernames=[];
 io.on('connection', function(socket){
-  connectCounter++;
-  console.log('Total '+connectCounter+' people are connected');
+ connectCounter++;
+ console.log('Total '+connectCounter+' people are connected');
  
-  var socketId = socket.id;
-  var clientIp = socket.request.connection.remoteAddress;
+ var socketId = socket.id;
+ var clientIp = socket.request.connection.remoteAddress;
 
-  console.log(clientIp);
+ console.log(clientIp);
 
 
-  
+
  
-  io.emit('totalusers',usernames, 'Total '+connectCounter+' people are connected');
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
+ io.emit('totalusers',usernames, 'Total '+connectCounter+' people are connected');
+ socket.on('chat message', function(msg){
+  console.log('message: ' + msg);
+ });
 
-  socket.on('adduser', function(username){
+ socket.on('adduser', function(username){
 		// we store the username in the socket session for this client
 		if(username==='' || username===null){
-                username = 'Guest'+Math.floor((Math.random() * 1000) + 1);
-                
-                }
-                socket.username = username;
-                usernames.push(username);
+   username = 'Guest'+Math.floor((Math.random() * 1000) + 1);
+
+  }
+  socket.username = username;
+  usernames.push(username);
 		// add the client's username to the global list
 		//usernames[username] = username;
 		// echo to client they've connected
@@ -68,37 +68,37 @@ io.on('connection', function(socket){
 	});
 
 
-socket.on('chat message', function(msg){
- var userName=socket.username;    
-if(clientIp=='127.0.0.1'){
+ socket.on('chat message', function(msg){
+  var userName=socket.username;    
+  if(clientIp=='127.0.0.1'){
      //userName='Admin';
-     }
+    }
     io.emit('chat message', userName,msg);
-  });
-fcdrhygrdtu7ggrm v7t8uy6y6trd
-  socket.on('disconnect', function(){
+   });
+
+ socket.on('disconnect', function(){
   connectCounter--;
   var index= usernames.indexOf(socket.username);
   console.log(socket.username);   
-if (index > -1) {dokghjpifiy
-    usernames.splice(index, 1);
-}
+  if (index > -1) {
+   usernames.splice(index, 1);
+  }
 
   io.emit('totalusers',usernames, 'Total '+connectCounter+' people are connected');
-  io.emit('disconnected', 'One of the user disconnected');
+  io.emit('disconnected', socket.username + ' has disconnected');
   
-  });
+ });
 });
 
 
 
-	
+
 });
 // Start the server
 server.start((err) => {
-yghujt666uhp0phycxcxcxcxcxvccxvcbcvnbhtr645hygs43ew
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+
+ if (err) {
+  throw err;
+ }
+ console.log('Server running at:', server.info.uri);
 });
